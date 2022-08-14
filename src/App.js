b72b786dab7d79957a8567da149670d2d498427f
adrian84 <ernesto.barraza216@gmail.com>
2022-08-14 16:22:28 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './componentes/Home'
+import ProductsProvider from './context/ProductContext';
+import NotFound from './componentes/NotFound';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ return (
+    <ProductsProvider>
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+    </ProductsProvider>
   );
 }
-
 export default App;
